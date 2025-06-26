1fadb9bbde0e9c89cc3e14c8705f5fe81d4a66b5
Sérgio Nunes <sagnunes@gmail.com>
2025-06-26 13:48:17 +0100

    refactor(roles): remove unused `create` method from `RoleController`

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -24,14 +24,6 @@ class RoleController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreRoleRequest $request): \Illuminate\Http\RedirectResponse
